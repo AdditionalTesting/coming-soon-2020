5e40326b83a96a3b70b2ef76d510ccc4e9d115c2
albert <albert@megaphonetv.com>
2020-06-15 21:14:14 -0400

    changed config.js to repo name

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-aerial/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/coming-soon-2020`, // This path is subpath of your hosting https://domain/portfolio
   // social
 
   authorName: 'Anubhav Srivastava',
